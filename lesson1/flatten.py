6ced570de22c02af051f5c356da80c56f451ab5e
Alex Sanchez <alex@s1ck.org>
2016-03-17 16:33:59 +0200

    minor update

--- lesson1/flatten.py
+++ lesson1/flatten.py
@@ -1,4 +1,8 @@
-input_list = [1, [2, 3], 4, [[6, 7]]]
+input_string = ''
+while not input_string.startswith('[') or not input_string.endswith(']'):
+    input_string = input('Enter a list: ')
+
+input_list = eval(input_string)
     
 def flatten(lst):
     if lst == []:
@@ -7,4 +11,4 @@ def flatten(lst):
         return flatten(lst[0]) + flatten(lst[1:])
     return lst[:1] + flatten(lst[1:])
 
-print(flatten(input_list))
+print('Result:', flatten(input_list))
