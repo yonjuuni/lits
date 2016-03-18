5ef19adb2283314f50528750174ebb9153935c11
Alex Sanchez <alex@s1ck.org>
2016-03-18 13:37:14 +0200

    minor pep8 update

--- lesson1/parentheses.py
+++ lesson1/parentheses.py
@@ -18,28 +18,28 @@ if open_par or close_par:
     if open_par == close_par:
         print('The number of (parentheses) is good to go.')
     elif open_par > close_par:
-        print('You are missing {} closing parentheses: )'.format(open_par - \
+        print('You are missing {} closing parentheses: )'.format(open_par -
                                                                  close_par))
     else:
-        print('You are missing {} opening parentheses: ('.format(close_par - \
+        print('You are missing {} opening parentheses: ('.format(close_par -
                                                                  open_par))
 
 if open_brackets or close_brackets:
     if open_brackets == close_brackets:
         print('The number of [brackets] is good to go.')
     elif open_brackets > close_brackets:
-        print('You are missing {} closing brackets: ]'.format(open_brackets - \
+        print('You are missing {} closing brackets: ]'.format(open_brackets -
                                                               close_brackets))
     else:
-        print('You are missing {} opening brackets: ['.format(close_brackets - \
+        print('You are missing {} opening brackets: ['.format(close_brackets -
                                                               open_brackets))
 
 if open_braces or close_braces:
     if open_braces == close_braces:
         print('The number of {braces} is good to go.')
     elif open_braces > close_braces:
-        print('You are missing {} closing braces: }}'.format(open_braces - \
+        print('You are missing {} closing braces: }}'.format(open_braces -
                                                              close_braces))
     else:
-        print('You are missing {} opening braces: {{'.format(close_braces - \
+        print('You are missing {} opening braces: {{'.format(close_braces -
                                                              open_braces))
